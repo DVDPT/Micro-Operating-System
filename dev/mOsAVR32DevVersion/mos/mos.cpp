0e09332bc161c585ca02ab7bceb1ed5ac9cea39e
Diogo Cardoso <amazing.red@gmail.com>
2011-10-03 18:35:59 +0100

    Created branch AVR32PORT. Added AVR Studio 5.0 project Added .gitignore

---
+++ dev/mOsAVR32DevVersion/mos/mos.cpp
@@ -0,0 +1,13 @@
+/*
+ * mos.cpp
+ *
+ * Created: 03-10-2011 18:25:17
+ *  Author: DVD
+ */ 
+
+#include <avr32/io.h>
+
+int function(void)
+{
+    //TODO:: Please write your application code 
+}
---
+++ dev/mOsAVR32DevVersion/mosTest/mosTest.cpp
@@ -0,0 +1,16 @@
+/*
+ * mosTest.cpp
+ *
+ * Created: 03-10-2011 18:25:44
+ *  Author: DVD
+ */ 
+
+#include <avr32/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
